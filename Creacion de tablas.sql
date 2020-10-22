fea54a5ac495d74978e324015b9d8ec9bbdb2a51
Gonzalo-Avila <goner@est.frba.utn.edu.ar>
2020-10-22 16:58:58 -0300

    Agrego un nuevo trigger de prueba

--- Creacion de tablas.sql
+++ Creacion de tablas.sql
@@ -171,7 +171,8 @@ GO
 
 CREATE TABLE BAD_QUERY.Logs(
   evento varchar(255),
-  fecha datetime
+  fecha datetime,
+  usuario sysname
 )
 GO 
 
@@ -358,7 +359,7 @@ GO
 CREATE TRIGGER BAD_QUERY.tr_log_nuevas_compras ON BAD_QUERY.Compras_automoviles AFTER INSERT
 AS
 BEGIN
-	INSERT INTO BAD_QUERY.Logs values ('Se han agregado nuevas compras de automoviles', GETDATE())
+	INSERT INTO BAD_QUERY.Logs values ('Se han agregado nuevas compras de automoviles', GETDATE(), CURRENT_USER)
 END
 GO
 
